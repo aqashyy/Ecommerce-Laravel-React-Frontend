e3ef3de46406e6912b0b6a1a7dfeb7a6947bc50c
aqashyy <ashiqmuhammedep@gmail.com>
2025-06-21 16:56:16 +0530

    delete category function setup completed

--- src/components/admin/category/Show.jsx
+++ src/components/admin/category/Show.jsx
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom'
 import { adminToken, apiUrl } from '../../common/http'
 import Loader from '../../common/Loader'
 import Nostate from '../../common/Nostate'
+import { toast } from 'react-toastify'
 
 function Show() {
 
     const [categories, setCategories] = useState([]);
     const [loader, setLoader] = useState(false);
-
+    // Function for fetching all categories
     const fetchCategories = async () => {
         setLoader(true);
         const res = await fetch(`${apiUrl}/categories`, {
@@ -31,6 +32,31 @@ function Show() {
                     console.log("something went wrong...");
                 }
             });
+    }
+
+    //  Delete category function
+    const deleteCategory = async (id) => {
+        if (confirm("Are you sure want to delete.?")) {
+            const res = await fetch(`${apiUrl}/categories/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${adminToken()}`
+                }
+            }).then(res => res.json())
+                .then(result => {
+
+                    if (result.status == 200) {
+                        // setCategories(result.data);
+                        const newCategories = categories.filter(category => category.id != id);
+                        setCategories(newCategories);
+                        toast.success(result.message);
+                    } else {
+                        console.log("something went wrong...");
+                    }
+                });
+        }
     }
     useEffect(() => {
         // console.log(adminToken());
@@ -92,7 +118,7 @@ function Show() {
                                                                         </path>
                                                                     </svg>
                                                                 </Link>
-                                                                <Link to="" className='text-danger ms-2'>
+                                                                <Link to="" className='text-danger ms-2' onClick={() => deleteCategory(category.id)}>
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                                                                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
                                                                     </svg>
